06cde9fd2cb09a5026692747a157189f52426e21
Tesalon Felicien <felicien.tesalon@gmail.com>
2021-01-13 14:41:36 -0500

    Added links

--- src/components/FooterView.js
+++ src/components/FooterView.js
@@ -1,16 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class FooterView extends Component {
-    render() {
-        return (
-            <footer>
-                <p className="boss">Sources: <a href="https://buildbackbetter.gov/the-transition/" target="_blank">The Transition </a> and <a href="https://www.whitehouse.gov/about-the-white-house/the-executive-branch/" target="_blank">The Executive Branch</a> 
-               </p>
-                    
-                    
-            </footer>
-        )
-    }
+	render() {
+		return (
+			<footer>
+				<p className="boss">
+					Sources:{' '}
+					<a
+						href="https://buildbackbetter.gov/the-transition/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						The Transition{' '}
+					</a>{' '}
+					and{' '}
+					<a
+						href="https://www.whitehouse.gov/about-the-white-house/the-executive-branch/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						The Executive Branch
+					</a>
+				</p>
+			</footer>
+		);
+	}
 }
 
-export default FooterView
+export default FooterView;
